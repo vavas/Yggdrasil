3f67aacfda23053ab3fe7602a9e54e0bc28eafef
vavas <vladimir.kryzhanovskyi@gmail.com>
2021-06-14 12:34:05 +0300

    init gRPC server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vavas/Yggdrasil
+
+go 1.15
+
+require google.golang.org/grpc v1.38.0
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
